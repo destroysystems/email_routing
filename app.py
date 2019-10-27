881c2ae5ae59bab4012d3f420f9d36f8726371c9
Maurício Dias <mau.dias@gmail.com>
2019-10-27 17:35:06 -0300

    [Mauricio] Parses request.form properly

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from email_routing import process
+from process import process
 
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def call():
-    if request.method == 'GET':
-        return 'Hi!'
-    elif request.method == 'POST':
-        return 'Success!'
+    return process(request)
